8f0602f79c607c6c06eb0251f7502597a53fbe5e
Leonardo Santos Silva <1532leleo@gmail.com>
2024-02-01 19:07:02 -0300

    Função para exibir informações do hotel e reserva apartir do id do hotel FEITA

--- index.js
+++ index.js
@@ -50,8 +50,6 @@ class Reserva {
     }
 }
 
-
-// - Um hotel deve possuir: id, nome, categoria, endereço e telefone;
 function CadastrarHotel() {
     let idHotel = parseInt(prompt("Insira o ID do hotel"))
     let nomeHotel = prompt("Insira o nome do hotel")
@@ -60,20 +58,62 @@ function CadastrarHotel() {
     let telefoneHotel = prompt("Insira o telefone do hotel")
 
     let hotel = new Hotel(idHotel, nomeHotel, categoriaHotel, enderecoHotel, telefoneHotel)
-    hoteis.push(hotel)
+
+
+    if (!hoteis.some(x => x.Id == idHotel)) {
+        hoteis.push(hotel)
+    } else {
+        console.log("Hotel com ID ja existente")
+    }
 }
-
-// - Uma reserva deve possuir: id, id do hotel, nome do responsável,
-
-//   dia de entrada e dia de saída;
 
 function CadastrarReserva() {
     let idReserva = parseInt(prompt("Insira o ID da reserva"))
     let idHotelReserva = parseInt(prompt("Insira o ID do hotel"))
     let nomeRespondavel = prompt("Insira o nome do responsável")
-    let diaEntrada = prompt("Insira o dia da entrada")
-    let diaSaida = prompt("Insira o dia da saída")
+    let diaEntrada = parseInt(prompt("Insira o dia da entrada"))
+    let diaSaida = parseInt(prompt("Insira o dia da saída"))
 
     let reserva = new Reserva(idReserva, idHotelReserva, nomeRespondavel, diaEntrada, diaSaida)
-    reservas.push(reserva)
+
+    if (diaEntrada < diaSaida) {
+
+        if (hoteis.some(x => x.Id == idHotelReserva)) {
+            reservas.push(reserva)
+        } else {
+            console.error("Hotel não existe no sistema")
+        }
+    } else {
+        console.error("Dia de entrada não pode ser maior que o dia de saída")
+    }
+}
+
+// 3 - Crie uma função que recebe como parâmetro o id do hotel e exibe na tela todas as reservas neste hotel com as seguintes informações: nome do hotel - nome do responsável da reserva - dia de entrada - dia de saída
+
+
+function ExibirReservas(idHotel) {
+
+    for (let index = 0; index < hoteis.length; index++) {
+        if (idHotel === hoteis[index].Id) {
+            for (let index2 = 0; index2 < reservas.length; index2++) {
+                if (hoteis[index].Id === reservas[index2].IdHotel) {
+                    console.log("Nome do hotel: " + hoteis[index].Nome)
+                    console.log("Nome do Responsável: " + reservas[index2].NomeResponsavel)
+                    console.log("Dia de entrada: " + reservas[index].DiaEntrada)
+                    console.log("Dia de saída: " + reservas[index].DiaSaida)
+                }
+            }
+        }
+    }
+}
+
+
+// 4 - Crie uma função que recebe como parâmetro o id de uma reserva e exibe no console: nome do hotel - endereço - dia de entrada - dia de saída
+
+
+function ExibirId(idReserva) {
+
+    for (let index = 0; index < array.length; index++) {
+
+    }
 }
